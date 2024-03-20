0b91dd5c10840c91bf1fcd58a32b647c6d9f696e
Renato Rojas <roj23813@uvg.edu.gt>
2024-03-19 18:25:11 -0600

    Se agrego la clase lector, para poder ingresar mas palabras al diccionario

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco();
+		Lector lector = new Lector();
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Configuraciones iniciales de palabras, según la hoja de trabajo: ");
 		banco.agregarPalabra("house", "casa");
@@ -9,13 +13,22 @@ public class Main {
 		banco.agregarPalabra("woman", "mujer");
 		banco.agregarPalabra("town", "pueblo");
 		banco.agregarPalabra("yes", "si");
-		
 		System.out.println("Se agregaron las palabras: house, dog, homework, woman, town, yes");
         
-        System.out.println(banco.getTraduccion("HOUSE"));
-		banco.getAllKeys();
 
+        System.out.println(banco.getTraduccion("HOUSE"));
+		banco.getAllWords();
 		
+		System.out.println("Indique el nombre del archivo que tiene el compendio de palabras y sus traducciones: ");
+		String archivo = scan.nextLine();
+		lector.leerArchivoBase(archivo);
+		for (int i = 0; i < lector.keys.length; i++) {
+			banco.agregarPalabra(lector.keys[i], lector.values[i]);			
+		}
+		banco.getAllWords();
+		
+		System.out.println(banco.getTraduccion("HELLO"));
+
 	}
 
     }
